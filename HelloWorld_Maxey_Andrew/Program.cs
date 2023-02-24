e9fe0637c991e9ef927f513b2de1a55e97cbd65e
KillaGoose51 <andrew.maxey825@gmail.com>
2023-02-24 15:09:53 -0600

    Initial commit

---
+++ HelloWorld_Maxey_Andrew/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld_Maxey_Andrew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld_Maxey_Andrew
+ 
